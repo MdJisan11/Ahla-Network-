7dc0981d69e8ff00a947e5da3256449584259da3
Md Mahmudul Hasan Jisan <mdjisantahinur464078@gmail.com>
2025-06-29 01:33:38 +0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import Hero from '../components/Hero';
-import Tokenomics from '../components/Tokenomics';
-import Ecosystem from '../components/Ecosystem';
-import Footer from '../components/Footer';
-export default function Home() {
-return (<div><Hero /><Tokenomics /><Ecosystem /><Footer /></div>);
-}
